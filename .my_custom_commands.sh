ff4489cd7780ee29d345a0f88efee5af2f37ce27
J0na555 <ethiopediaa@gmail.com>
2025-05-21 09:21:53 +0300

    tried to commit the read me again but it wont work

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -2,15 +2,24 @@
 
 function create() {
     source .env
+
+
     source ~/Documents/projects/pyAuto/pypy/bin/activate
     python pyauto.py $1
-    cd $FILEPATH$1
+
+    cd $FILEPATH$1 || exit
+
     git init
     git remote add origin git@github.com:$USERNAME/$1.git
-    touch README.md
+
+    # touch README.md
+    echo "# $1" > README.md
+
     git add README.md
     git commit -m "Initial commit"
+    git branch -M main
     git push -u origin main
+
 
     code .
 }
